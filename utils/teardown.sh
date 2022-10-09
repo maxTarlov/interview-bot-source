687669d8279a998a9bfcf9861cd39d48bb50f11c
Max Tarlov <maxtarlov@gmail.com>
2022-10-09 14:40:21 -0700

    fix teardown deactivate call

--- utils/teardown.sh
+++ utils/teardown.sh
@@ -1,4 +1,6 @@
-INTERVIEW_BOT_SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+INTERVIEW_BOT_SOURCE_LINK=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+# Linux requires readlink -f, OSX requires that there be no options smh
+INTERVIEW_BOT_SOURCE_DIR=$( readlink -f $INTERVIEW_BOT_SOURCE_LINK || readlink $INTERVIEW_BOT_SOURCE_LINK )
 
 if [[ $(which python) == "$INTERVIEW_BOT_SOURCE_DIR/.venv/bin/python" ]]; then 
     deactivate
