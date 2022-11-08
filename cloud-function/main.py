7be850cb51a0c77660cebdec3305e7ae974099d9
Max Tarlov <maxtarlov@gmail.com>
2022-11-08 13:53:17 -0800

    add user feedback

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -3,9 +3,10 @@ import functions_framework
 
 from google.cloud import firestore
 db = firestore.Client()
-question_logs = db.collection(u'question-logs')
+question_logs = db.collection('question-logs')
+user_feedback = db.collection('user-feedback')
 
-ALLOW_METHODS = 'OPTIONS, GET'
+ALLOW_METHODS = 'OPTIONS, GET, POST'
 
 DEFAULT_QUESTION = 'Tell me about yourself.'
 DEFAULT_ANSWER = matcher.question_answer_mappings[DEFAULT_QUESTION]
@@ -46,6 +47,25 @@ def handle_get(request):
     body['id'] = log_ref.id  # send unique id for feedback
     return body, 200, headers
 
+def handle_post(request):
+    """
+    POST requests are to provide feedback on the bot's response to a previous question.
+    Expect a firestore document ID which corresponds to the ID of the question log.
+    """
+
+    headers = {'Access-Control-Allow-Origin': '*'}
+    try:
+        request_json = request.get_json()
+    except Exception as inst:
+        return inst.description, 400, headers
+    if 'id' in request_json and 'good' in request_json:
+        user_feedback.document(str(request_json['id']))\
+            .set({'good': request_json['good']})
+        return '', 204, headers
+    else:
+        return 'Expected keys "id" and "good" in JSON', 400, headers
+        
+
 @functions_framework.http
 def route_requests(request):
     # For more information about CORS and CORS preflight requests, see:
@@ -60,5 +80,7 @@ def route_requests(request):
         'Access-Control-Max-Age': '86400'
         }
         return ('', 204, headers)
+    elif request.method == 'POST':
+        return handle_post(request)
     else:
         return handle_get(request)
